acceeee217485802bedc6048fefb5bad4ae0d5fc
Home Therapy Locator <hometherapylocator@gmail.com>
2019-06-17 19:01:39 -0400

    Adding search function

--- src/app.js
+++ src/app.js
@@ -107,6 +107,11 @@ const mapStyle = [
   }
 ];
 
+var map;
+var markers = [];
+var infoWindow;
+var locationSelect;
+
 // Escapes HTML characters in a template literal string, to prevent XSS.
 // See https://www.owasp.org/index.php/XSS_%28Cross_Site_Scripting%29_Prevention_Cheat_Sheet#RULE_.231_-_HTML_Escape_Before_Inserting_Untrusted_Data_into_HTML_Element_Content
 function sanitizeHTML(strings) {
@@ -142,9 +147,19 @@ function initMap() {
       }
     };
   });
+  
+  searchButton = document.getElementById("searchButton").onclick = searchLocations;
 
+  locationSelect = document.getElementById("locationSelect");
+          locationSelect.onchange = function() {
+            var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
+            if (markerNum != "none"){
+              google.maps.event.trigger(markers[markerNum], 'click');
+            }
+          };
+  
   const apiKey = 'AIzaSyDoYISVblufYSzI0no4WIpkN5QHLSCwJgU';
-  const infoWindow = new google.maps.InfoWindow();
+  infoWindow = new google.maps.InfoWindow();
   infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
 
   // Show the information for a store when its marker is clicked.
@@ -170,4 +185,107 @@ function initMap() {
     infoWindow.open(map);
   });
 
+}
+
+ function searchLocations() {
+  var address = document.getElementById("addressInput").value;
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({address: address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+    searchLocationsNear(results[0].geometry.location);
+    } else {
+      alert(address + ' not found');
+    }
+  });
+}
+
+function clearLocations() {
+  infoWindow.close();
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers.length = 0;
+
+  locationSelect.innerHTML = "";
+  var option = document.createElement("option");
+  option.value = "none";
+  option.innerHTML = "See all results:";
+  locationSelect.appendChild(option);
+}
+
+function searchLocationsNear(center) {
+  clearLocations();
+
+  var radius = 500;
+  var searchUrl = 'index.html?lat=' + center.lat() + '&lng=' + center.lng() + '&radius=' + radius;
+  downloadUrl(searchUrl, function(data) {
+    var xml = parseXml(data);
+    var markerNodes = xml.documentElement.getElementsByTagName("marker");
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < markerNodes.length; i++) {
+      var id = markerNodes[i].getAttribute("id");
+      var name = markerNodes[i].getAttribute("name");
+      var address = markerNodes[i].getAttribute("address");
+      var distance = parseFloat(markerNodes[i].getAttribute("distance"));
+      var latlng = new google.maps.LatLng(
+          parseFloat(markerNodes[i].getAttribute("lat")),
+          parseFloat(markerNodes[i].getAttribute("lng")));
+
+      createOption(name, distance, i);
+      createMarker(latlng, name, address);
+      bounds.extend(latlng);
+    }
+    map.fitBounds(bounds);
+    locationSelect.style.visibility = "visible";
+    locationSelect.onchange = function() {
+      var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
+      google.maps.event.trigger(markers[markerNum], 'click');
+    };
+  });
+}
+
+function createMarker(latlng, name, address) {
+  var html = "<b>" + name + "</b> <br/>" + address;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: latlng
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+  markers.push(marker);
+}
+
+function createOption(name, distance, num) {
+  var option = document.createElement("option");
+  option.value = num;
+  option.innerHTML = name;
+  locationSelect.appendChild(option);
+}
+
+function downloadUrl(url, callback) {
+  var request = window.ActiveXObject ?
+      new ActiveXObject('Microsoft.XMLHTTP') :
+      new XMLHttpRequest;
+
+  request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+      request.onreadystatechange = doNothing;
+      callback(request.responseText, request.status);
+    }
+  };
+
+  request.open('GET', url, true);
+  request.send(null);
+}
+
+function parseXml(str) {
+  if (window.ActiveXObject) {
+    var doc = new ActiveXObject('Microsoft.XMLDOM');
+    doc.loadXML(str);
+    return doc;
+  } else if (window.DOMParser) {
+    return (new DOMParser).parseFromString(str, 'text/xml');
+  }
 }
